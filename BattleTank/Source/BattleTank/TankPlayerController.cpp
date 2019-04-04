4a4d71f14855b18090072749e000b4aa0d0a6553
DESKTOP-534M98F\BraidedMantis7 <chavezgabe777@gmail.com>
2019-04-04 03:23:47 -0700

    Using Virtual And Overload

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -1,11 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerControlller not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerControlller possesing %s"), *(ControlledTank->GetName()));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("PlayerControlller Begin Play"));
+}
 
 ATank* ATankPlayerController :: GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+
