c7571b59e3cd241c861a83cc8b2df447b39a13b6
DESKTOP-534M98F\BraidedMantis7 <chavezgabe777@gmail.com>
2019-04-09 02:25:57 -0700

    AddTick() To Player Controller

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -4,6 +4,9 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+
+
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -19,9 +22,25 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+// Tick
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	//Super
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+	UE_LOG(LogTemp, Warning, TEXT("What is Love!"));
+}
+
 ATank* ATankPlayerController :: GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController :: AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
 
+	//Get World Location through cross hair (Line Trace)
+	//if it hits the landscape 
+		// Tell Controlled tank to aim at this point
+}
