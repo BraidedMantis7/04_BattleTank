5e84eec079600a23ec0bc98c87835b3d25a6fd54
DESKTOP-534M98F\BraidedMantis7 <chavezgabe777@gmail.com>
2019-04-24 02:11:37 -0700

    Using Clamp() To Limit Values

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -57,6 +57,6 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
-	Barrel->Elevate(5); // TODO remove Magic Number
+	Barrel->Elevate(DeltaRotator.Pitch); // TODO remove Magic Number
 
 }
